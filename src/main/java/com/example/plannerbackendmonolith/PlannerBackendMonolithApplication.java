ae462ea05d135a662db1ab141c4cd2ecaaff9da8
MishailEx <mishail.avram@mail.ru>
2022-11-27 20:15:03 +0300

    init commit

---
+++ src/main/java/com/example/plannerbackendmonolith/PlannerBackendMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.example.plannerbackendmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerBackendMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerBackendMonolithApplication.class, args);
+    }
+
+}
